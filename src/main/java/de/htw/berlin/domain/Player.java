3a543253771627d3aa04d31cc18cd3e3952a49d8
C-Emrhan <77165697+C-Emrhan@users.noreply.github.com>
2022-10-19 14:37:26 +0200

    Player.class

--- src/main/java/de/htw/berlin/domain/Player.java
+++ src/main/java/de/htw/berlin/domain/Player.java
@@ -4,14 +4,36 @@ import java.util.List;
 
 public class Player {
     private String name;
-    private Stack hand;
+    private List<Card> hand;
     private boolean saidMauMau = false;
 
+
+
+    public boolean isSaidMauMau() {
+        return saidMauMau;
+    }
+
+    public void setSaidMauMau(boolean saidMauMau) {
+        this.saidMauMau = saidMauMau;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
 
     boolean playCard(Card card){
         return false;
     }
-
     void drawCard(){}
 
 
