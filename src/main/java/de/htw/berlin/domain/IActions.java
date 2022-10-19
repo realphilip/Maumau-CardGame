824b7395ba439502dd24fc11454638589098bdf8
Daniel Edlich <s0578291@htw-berlin.de>
2022-10-19 15:06:15 +0200

    Create JavaDoc

--- src/main/java/de/htw/berlin/domain/IActions.java
+++ src/main/java/de/htw/berlin/domain/IActions.java
@@ -6,22 +6,53 @@ public interface IActions {
 
     /**
      *
-     * @return
+     * Draws a card from the drawStack
+     *
+     * @return Card
      */
-    Card drawCard(int howManytoDraw); //define how many
+    Card drawCard(int howManyToDraw);
 
     /**
      * Play a card
-     * Card must be playable
      * @param card The card is played
-     * @return If playing was successful
+     * @param color The color of the played card
+     * @return If card is playable
      */
     boolean playCard(Card card, Color color); //Jack cant be played on another Jack // consider Stack and Player
 
+    /**
+     *
+     * Shuffel a Stack
+     *
+     * @param stack of cards
+     *
+     * @return shuffled Stack
+     */
+    Stack shuffleStack(Stack stack);
+
+    /**
+     *
+     * Announce the winner of a Game
+     *
+     * @param player that won.
+     */
+    void announceWinner(Player player);
+
+    /**
+     *
+     * @param color
+     */
     void wishColor (Color color);
+
+    /**
+     * Skip the next player
+     */
     void skipTurn();
+
+    /**
+     *
+     * @return If MauMau is valid
+     */
     boolean sayMauMau();
-    void shuffleStack(List<Card> cardList);
-    void announceWinner();
 
 }
