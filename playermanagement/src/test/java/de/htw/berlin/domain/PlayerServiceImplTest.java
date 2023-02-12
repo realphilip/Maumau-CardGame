bcfaca7da847a04844cf542e856842dfd06a1860
Canerelpacino <canersmedia@gmail.com>
2023-02-12 19:54:55 +0100

    edit impl tests

--- playermanagement/src/test/java/de/htw/berlin/domain/PlayerServiceImplTest.java
+++ playermanagement/src/test/java/de/htw/berlin/domain/PlayerServiceImplTest.java
@@ -1,69 +1,52 @@
-package de.htw.berlin.domain;import de.htw.berlin.domain.Card;
-import de.htw.berlin.impl.PlayerServiceImpl;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+package de.htw.berlin.domain;
 
 import de.htw.berlin.domain.Card;
 import de.htw.berlin.domain.Player;
 import de.htw.berlin.domain.Stack;
 import de.htw.berlin.export.PlayerService;
+import de.htw.berlin.impl.PlayerServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class PlayerServiceImplTest {
-    @Mock
-    private Stack hand;
+    @InjectMocks
+    private PlayerServiceImpl playerServiceImpl;
 
     @Mock
     private Card card;
 
-    @InjectMocks
-    private PlayerServiceImpl playerServiceImpl;
-
     @Test
     public void testNewPlayer() {
-        String name = "player1";
-        Player expectedPlayer = new Player();
-        expectedPlayer.setName(name);
-
-        Player actualPlayer = playerServiceImpl.newPlayer(name);
-
-        assertEquals(expectedPlayer, actualPlayer);
+        Player player = playerServiceImpl.newPlayer("John");
+        Assert.assertEquals("John", player.getName());
     }
 
     @Test
     public void testAddCardtoHand() {
-        Player player = new Player();
+        Player player = playerServiceImpl.newPlayer("John");
+        Stack hand = new Stack();
+        hand.cardList = new ArrayList<>();
         player.setHand(hand);
-
-        List<Card> cardList = new ArrayList<>();
-        when(hand.getCardList()).thenReturn(cardList);
-
-        Player expectedPlayer = playerServiceImpl.addCardtoHand(card, player);
-
-        assertEquals(expectedPlayer.getHand().getCardList().size(), 1);
+        player = playerServiceImpl.addCardtoHand(card, player);
+        Assert.assertEquals(1, player.getHand().cardList.size());
     }
 
     @Test
     public void testRemoveCardfromHand() {
-        Player player = new Player();
+        Player player = playerServiceImpl.newPlayer("John");
+        Stack hand = new Stack();
+        hand.cardList = new ArrayList<>();
+        hand.cardList.add(card);
         player.setHand(hand);
-
-        List<Card> cardList = new ArrayList<>();
-        cardList.add(card);
-        when(hand.getCardList()).thenReturn(cardList);
-
-        Player expectedPlayer = playerServiceImpl.removeCardfromHand(card, player);
-
-        assertEquals(expectedPlayer.getHand().getCardList().size(), 0);
+        player = playerServiceImpl.removeCardfromHand(card, player);
+        Assert.assertEquals(0, player.getHand().cardList.size());
     }
 }
